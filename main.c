9000e2a1717689bc039846941f9a9f08c0a4c3a6
savinu-pixel <fernandosavinusharon@gmail.com>
2026-07-17 09:27:11 +0530

    Add menu

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+
+
 int main(){
+	loadHighScores();
+	menu();	
 	return 0;
+}
+
+void menu(){
+	int choice;
+	do{
+		system("cls"): //clears the screen
+		
+		printf("************************\n");
+        printf("       PONG GAME\n");
+        printf("************************\n");
+		printf("========================\n");
+        printf("1. Start Game\n");
+        printf("2. View Instructions\n");
+        printf("3. View High Scores\n");
+        printf("4. Exit\n");
+		printf("========================\n");
+        printf("************************\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+		
+		
+	}while(choice != 4)
 }
